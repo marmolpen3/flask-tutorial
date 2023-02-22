7afca586f93d9d315a5ac23f382f2e1fc155fef4
marmolpen3 <marmolpen3@alum.us.es>
2023-02-22 13:30:57 +0100

    Añadir documentación para el ejemplo

--- expecific_middleware/app.py
+++ expecific_middleware/app.py
@@ -1,7 +1,21 @@
 from flask import Flask, g
 from middleware import hello_middleware
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask('app')
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.json'  # Our API url (can of course be a local resource)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 # This is a decorator that tells Flask what URL should trigger our function. 
 @app.get('/')
